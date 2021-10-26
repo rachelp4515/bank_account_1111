3d268ef224bac7d01429ada21902ecfa992792ef
rachelp4515 <rachel.porhammer@students.dominican.edu>
2021-10-26 15:58:30 -0400

    all functions working

--- BankAccount.py
+++ BankAccount.py
@@ -8,8 +8,6 @@ class BankAccount:
         after_depo_amount = amount + self.balance
         print(f'Amount deposited: ${amount}\n new balance: ${after_depo_amount}')
         return after_depo_amount
-        #adds amount to balance
-        #“Amount deposited: $X.XX new balance: $Y.YY”
 
     def withdraw(self, amount):
         if self.balance > amount:
@@ -17,29 +15,31 @@ class BankAccount:
             print(f'Amount deposited: ${amount}\n new balance: ${after_wd_amount}')
         else:
             print(f'Insufficient funds. $10 overdraft fee has been charged to your account. Current Balance:{self.balance - 10 } ')
-        #subtracts amount from balance
-        #“Amount withdrawn: $X.XX new balance: $Y.YY”
-        #user tries to withdraw an amount greater than balance, ”Insufficient funds.”
-        #overdraft fee ($10)
-        pass
 
     def get_balance(self):
         print(f'Your balance is currently {self.balance}')
         return self.balance
 
     def add_interest(self, months):
-
+        interest = months * 0.00083
+        print(f'Current balance plus interest: {self.balance + interest}')
+        
         #adds interest to balance
         #monthly interest (interest = balance *  0.00083 )
         pass
     
     def print_statement(self):
+        num = self.account_number[4:8]
+        print(f'{self.full_name}\nAccount No.: ****{num}\nBalance: ${self.balance}')
         #Joi Anderson
         #Account No.: ****5678
         #Balance: $100.00
-        pass
 
-testAccount = BankAccount("Rae Porhammer", 86753090, 37.90)
+testAccount = BankAccount("Rae Porhammer", "86753592", 37.90)
 #testAccount.deposit(23.17)
 #testAccount.withdraw(42)
-testAccount.withdraw(7.75)
+#testAccount.withdraw(7.75)
+#testAccount.get_balance()
+#testAccount.add_interest(14)
+testAccount.print_statement()
+
